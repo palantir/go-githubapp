0d14882f1ba19be16dd99fe2893184701a7d7592
Billy Keyes <bkeyes@palantir.com>
2021-07-23 21:33:15 +0000

    Implement loading algorithm for appconfig

--- appconfig/appconfig.go
+++ appconfig/appconfig.go
@@ -1,3 +1,17 @@
+// Copyright 2021 Palantir Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package appconfig loads repository configuration for GitHub apps. It
 // supports loading directly from a file in a repository, loading from remote
 // references, and loading an organization-level default. The config itself can
@@ -6,9 +20,14 @@ package appconfig
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"strings"
 
 	"github.com/google/go-github/v37/github"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
 )
 
 // RemoteRefParser attempts to parse a RemoteRef from bytes. The parser should
@@ -27,6 +46,14 @@ type RemoteRef struct {
 	// The reference (branch, tag, or SHA) to read in the repository. If empty,
 	// use the default branch of the repository.
 	Ref string `yaml:"ref" json:"ref"`
+}
+
+func (r RemoteRef) SplitRemote() (owner, repo string, err error) {
+	slash := strings.IndexByte(r.Remote, '/')
+	if slash <= 0 || slash >= len(r.Remote)-1 {
+		return "", "", errors.Errorf("invalid remote value: %s", r.Remote)
+	}
+	return r.Remote[:slash], r.Remote[slash+1:], nil
 }
 
 // Config contains unparsed configuration data and metadata about where it was found.
@@ -50,7 +77,7 @@ type Loader struct {
 
 	parser       RemoteRefParser
 	defaultRepo  string
-	defaultPaths string
+	defaultPaths []string
 }
 
 // NewLoader creates a Loader that loads configuration from paths.
@@ -82,19 +109,191 @@ func NewLoader(paths []string, opts ...Option) *Loader {
 //
 // If error is non-nil, the Source and Path fields of the returned Config tell
 // which file LoadConfig was processing when it encountered the error.
-func (ld *Loader) LoadConfig(ctx context.Context, client *github.Client, owner, repo string) (Config, error) {
-	// for each path:
-	//   try loading
-	//   if exists:
-	//		try parsing as remote
-	//		if remote:
-	//		  load remote
-	//		else:
-	//		  return
-	//
-	// for each default path:
-	//   try loading
-	//   if exists:
-	//     return
-	panic("TODO(bkeyes): implement this")
+func (ld *Loader) LoadConfig(ctx context.Context, client *github.Client, owner, repo, ref string) (Config, error) {
+	logger := zerolog.Ctx(ctx)
+
+	c := Config{
+		Source: fmt.Sprintf("%s/%s@%s", owner, repo, ref),
+	}
+
+	for _, p := range ld.paths {
+		c.Path = p
+
+		logger.Debug().Msgf("Trying configuration at %s in %s", c.Path, c.Source)
+		content, exists, err := getFileContents(ctx, client, owner, repo, ref, p)
+		if err != nil {
+			return c, err
+		}
+		if !exists {
+			continue
+		}
+
+		// if remote refs are enabled, see if the file is a remote reference
+		if ld.parser != nil {
+			remote, err := ld.parser(p, content)
+			if err != nil {
+				return c, err
+			}
+			if remote != nil {
+				logger.Debug().Msgf("Found remote configuration at %s in %s", p, c.Source)
+				return ld.loadRemoteConfig(ctx, client, *remote, c)
+			}
+		}
+
+		// non-remote content found, don't try any other paths
+		logger.Debug().Msgf("Found configuration at %s in %s", c.Path, c.Source)
+		c.Content = content
+		return c, nil
+	}
+
+	// if the repository defined no configuration and org defaults are enabled,
+	// try falling back to the defaults
+	if ld.defaultRepo != "" && len(ld.defaultPaths) > 0 {
+		return ld.loadDefaultConfig(ctx, client, owner)
+	}
+
+	// couldn't find configuration anyhere, so return an empty/undefined one
+	return Config{}, nil
+}
+
+func (ld *Loader) loadRemoteConfig(ctx context.Context, client *github.Client, remote RemoteRef, c Config) (Config, error) {
+	logger := zerolog.Ctx(ctx)
+
+	owner, repo, err := remote.SplitRemote()
+	if err != nil {
+		return c, err
+	}
+
+	c.Path = remote.Path
+	c.Source = fmt.Sprintf("%s/%s@%s", owner, repo, remote.Ref)
+	c.IsRemote = true
+
+	logger.Debug().Msgf("Trying remote configuration at %s in %s", c.Path, c.Source)
+	content, exists, err := getFileContents(ctx, client, owner, repo, remote.Ref, remote.Path)
+	if err != nil {
+		return c, err
+	}
+	if !exists {
+		// Referencing a remote file that does not exist is an error because
+		// this condition is annoying to debug otherwise. From the perspective
+		// of a repository, it appears that the application has a configuration
+		// file and it is easy to miss that e.g. the ref is wrong.
+		return c, errors.Errorf("invalid remote reference: file does not exist")
+	}
+
+	c.Content = content
+	return c, nil
+}
+
+func (ld *Loader) loadDefaultConfig(ctx context.Context, client *github.Client, owner string) (Config, error) {
+	logger := zerolog.Ctx(ctx)
+
+	r, _, err := client.Repositories.Get(ctx, owner, ld.defaultRepo)
+	if err != nil {
+		if isNotFound(err) {
+			// if the owner has no default repo, return empty/undefined config
+			return Config{}, nil
+		}
+		c := Config{Source: fmt.Sprintf("%s/%s", owner, ld.defaultRepo)}
+		return c, errors.Wrap(err, "failed to get default repository")
+	}
+
+	ref := r.GetDefaultBranch()
+	c := Config{
+		Source: fmt.Sprintf("%s/%s@%s", owner, r.GetName(), ref),
+	}
+
+	for _, p := range ld.defaultPaths {
+		c.Path = p
+
+		logger.Debug().Msgf("Trying default configuration at %s in %s", c.Path, c.Source)
+		content, exists, err := getFileContents(ctx, client, owner, r.GetName(), ref, p)
+		if err != nil {
+			return c, err
+		}
+		if !exists {
+			continue
+		}
+
+		// non-remote content found, don't try any other paths
+		logger.Debug().Msgf("Found default configuration at %s in %s", c.Path, c.Source)
+		c.Content = content
+		return c, nil
+	}
+
+	// no default configuration, return an empty/undefined one
+	return Config{}, nil
+}
+
+// getFileContents returns the content of the file at path on ref in owner/repo
+// if it exists. Returns an empty slice and false if the file does not exist.
+func getFileContents(ctx context.Context, client *github.Client, owner, repo, ref, path string) ([]byte, bool, error) {
+	file, _, _, err := client.Repositories.GetContents(ctx, owner, repo, path, &github.RepositoryContentGetOptions{
+		Ref: ref,
+	})
+	if err != nil {
+		switch {
+		case isNotFound(err):
+			return nil, false, nil
+		case isTooLargeError(err):
+			b, err := getLargeFileContents(ctx, client, owner, repo, ref, path)
+			return b, true, err
+		}
+		return nil, false, errors.Wrap(err, "failed to read file")
+	}
+
+	// file will be nil if the path exists but is a directory
+	if file == nil {
+		return nil, false, nil
+	}
+
+	content, err := file.GetContent()
+	if err != nil {
+		return nil, true, errors.Wrap(err, "failed to decode file content")
+	}
+
+	return []byte(content), true, nil
+}
+
+// getLargeFileContents is similar to getFileContents, but works for files up
+// to 100MB. Unlike getFileContents, it returns an error if the file does not
+// exist.
+func getLargeFileContents(ctx context.Context, client *github.Client, owner, repo, ref, path string) ([]byte, error) {
+	body, res, err := client.Repositories.DownloadContents(ctx, owner, repo, path, &github.RepositoryContentGetOptions{
+		Ref: ref,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read file")
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("failed to read file: unexpected status code %d", res.StatusCode)
+	}
+
+	defer func() {
+		_ = body.Close()
+	}()
+
+	b, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read file")
+	}
+	return b, nil
+}
+
+func isNotFound(err error) bool {
+	if rerr, ok := err.(*github.ErrorResponse); ok {
+		return rerr.Response.StatusCode == http.StatusNotFound
+	}
+	return false
+}
+
+func isTooLargeError(err error) bool {
+	if rerr, ok := err.(*github.ErrorResponse); ok {
+		for _, err := range rerr.Errors {
+			if err.Code == "too_large" {
+				return true
+			}
+		}
+	}
+	return false
 }
--- appconfig/options.go
+++ appconfig/options.go
@@ -1,8 +1,18 @@
-package appconfig
+// Copyright 2021 Palantir Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import (
-	"github.com/palantir/go-githubapp/githubapp"
-)
+package appconfig
 
 type Option func(*Loader)
 
@@ -21,16 +31,25 @@ func WithRemoteRefParser(parser RemoteRefParser) Option {
 // owner defaults.
 func WithOwnerDefault(name string, paths []string) Option {
 	return func(ld *Loader) {
-		ld.defaultName = name
+		ld.defaultRepo = name
 		ld.defaultPaths = paths
 	}
 }
+
+/*
+
+Not sure this is valuable yet, but leaving this option function as a starting
+point for a future implementation. See https://github.com/palantir/policy-bot/issues/111
+for some explanation of why this is desired.
+
+In the Loader implementation, if a ClientCreator and InstallationsService are
+set, the loadRemoteConfig method would use them to create a new client if the
+remote owner does not equal the starting owner.
 
 // WithPrivateRemotes enables loading remote configuration from private
 // repositories in different organizations. By default, only public
 // repositories can be remote targets.
 func WithPrivateRemotes(cc githubapp.ClientCreator, installs githubapp.InstallationsService) Option {
-	// TODO(bkeyes): implement this, if this functionality is valuable
-	// See https://github.com/palantir/policy-bot/issues/111
-	panic("TODO(bkeyes): unimplemented")
+	return func(ld *Loader) {}
 }
+*/
