75e00deb8d8ba738ca171e0d1381c563fe0b7285
asvoboda <svoboda.andrew@gmail.com>
2023-06-12 10:35:13 -0400

    Update go-github to v53

--- appconfig/appconfig_test.go
+++ appconfig/appconfig_test.go
@@ -21,7 +21,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 )
 
 const (
--- githubapp/context_test.go
+++ githubapp/context_test.go
@@ -20,7 +20,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 	"github.com/rs/zerolog"
 )
 
