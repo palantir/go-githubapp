e770082f3e7c77b13095939715345d158f09eb5a
Andrew Svoboda <svoboda.andrew@gmail.com>
2024-02-26 19:38:48 +0000

    Update go-github to v59 (#334)

--- appconfig/appconfig_test.go
+++ appconfig/appconfig_test.go
@@ -21,7 +21,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/google/go-github/v58/github"
+	"github.com/google/go-github/v59/github"
 )
 
 const (
--- githubapp/context_test.go
+++ githubapp/context_test.go
@@ -20,7 +20,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/google/go-github/v58/github"
+	"github.com/google/go-github/v59/github"
 	"github.com/rs/zerolog"
 )
 
