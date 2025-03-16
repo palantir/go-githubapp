10667f9a880200d1b629b24b5b817438ba712df1
Andy Grunwald <andygrunwald@gmail.com>
2025-03-16 11:07:27 +0100

    Metrics: Add X-Ratelimit-Used as metric

--- githubapp/middleware.go
+++ githubapp/middleware.go
@@ -34,6 +34,7 @@ const (
 
 	MetricsKeyRateLimit          = "github.rate.limit"
 	MetricsKeyRateLimitRemaining = "github.rate.remaining"
+	MetricsKeyRateLimitUsed      = "github.rate.used"
 )
 
 // ClientMetrics creates client middleware that records metrics about all
@@ -73,11 +74,13 @@ func ClientMetrics(registry metrics.Registry) ClientMiddleware {
 
 				limitMetric := fmt.Sprintf("%s[installation:%d]", MetricsKeyRateLimit, installationID)
 				remainingMetric := fmt.Sprintf("%s[installation:%d]", MetricsKeyRateLimitRemaining, installationID)
+				usedMetric := fmt.Sprintf("%s[installation:%d]", MetricsKeyRateLimitUsed, installationID)
 
 				// Headers from https://developer.github.com/v3/#rate-limiting
 				updateRegistryForHeader(res.Header, httpHeaderRateLimit, metrics.GetOrRegisterGauge(limitMetric, registry))
 				updateRegistryForHeader(res.Header, httpHeaderRateRemaining, metrics.GetOrRegisterGauge(remainingMetric, registry))
-				// TODO Think about to add X-Ratelimit-Used, X-Ratelimit-Reset and X-Ratelimit-Resource as well
+				updateRegistryForHeader(res.Header, httpHeaderRateUsed, metrics.GetOrRegisterGauge(usedMetric, registry))
+				// TODO Think about to add X-Ratelimit-Reset and X-Ratelimit-Resource as well
 			}
 
 			return res, err
